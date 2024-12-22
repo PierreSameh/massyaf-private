95a1d96a478120aa66f1208333fc721c381dc321
PierreSameh <captainpirzon@gmail.com>
2024-12-22 15:42:55 +0200

    city crud for admin

--- routes/admin.php
+++ routes/admin.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\AuthController;
+use App\Http\Controllers\Admin\CityController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,4 +9,7 @@ Route::prefix("admin")->group(function () {
     //Auth
     Route::post('/login', [AuthController::class, 'login']);
 
+    Route::middleware('auth:sanctum,admin')->group(function () {
+        Route::apiResource('city', CityController::class);
+    });
 });
