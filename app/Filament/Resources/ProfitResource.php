d0e88894b587532c9cb3ac5f0e58775b98fd728d
PierreSameh <captainpirzon@gmail.com>
2025-01-18 17:13:59 +0200

    profits resource

---
+++ app/Filament/Resources/ProfitResource.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ProfitResource\Pages;
+use App\Filament\Resources\ProfitResource\RelationManagers;
+use App\Models\Profit;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ProfitResource extends Resource
+{
+    protected static ?string $model = Profit::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\Select::make('type')
+                    ->label(__("Type"))
+                    ->options([
+                        "unit" => __("Unit"),
+                        "hotel" => __("Hotel"),
+                    ])
+                    ->required(),
+                Forms\Components\TextInput::make('from')
+                    ->label(__("Price From"))
+                    ->required()
+                    ->suffix(__('EGP'))
+                    ->numeric(),
+                Forms\Components\TextInput::make('to')
+                    ->label(__("Price To"))
+                    ->suffix(__('EGP'))
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('percentage')
+                    ->label(__('Percentage of booking value'))
+                    ->required()
+                    ->suffix('%')
+                    ->numeric(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('type')->label(__("Type")),
+                Tables\Columns\TextColumn::make('from')
+                    ->label(__("Price From"))
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('to')
+                    ->label(__("Price To"))
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('percentage')
+                    ->label(__('Percentage of booking value'))
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                Tables\Filters\SelectFilter::make('type')
+                    ->options([
+                        "unit" => __("Unit"),
+                        "hotel" => __("Hotel"),
+                    ]),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProfits::route('/'),
+            'create' => Pages\CreateProfit::route('/create'),
+            'edit' => Pages\EditProfit::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ database/migrations/2025_01_18_170456_create_profits_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('profits', function (Blueprint $table) {
+            $table->id();
+            $table->enum('type', ['unit', 'hotel']);
+            $table->decimal('from', 8, 2);
+            $table->decimal('to',8,2);
+            $table->decimal('percentage',8,2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profits');
+    }
+};
