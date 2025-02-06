5822d50856faccfa070c58de10bfe877962549e3
Kareem Mohamed <kotbekareem74@gmail.com>
2025-02-06 11:49:34 +0200

    edit city model error at 41

--- app/Models/City.php
+++ app/Models/City.php
@@ -17,7 +17,7 @@ class City extends Model
         'features',
         'coordinates'
     ];
-    
+
 
     protected $casts = [
         'images' => 'array',
@@ -30,7 +30,7 @@ class City extends Model
     public function toArray()
     {
         $attributes = parent::toArray();
-        
+
         foreach ($this->translatable as $field) {
             $attributes[$field] = $this->getTranslation($field, app()->getLocale());
         }
@@ -38,30 +38,36 @@ class City extends Model
         return $attributes;
     }
 
-    public function getImagesAttribute($value){
-        $images = json_decode($value, true);
+    public function getImagesAttribute($value)
+    {
+        $images = json_decode($value, true) ?? []; // Ensure $images is always an array
         $json = [];
         $isApiRequest = str_contains(request()->path(), 'api');
-        if($isApiRequest){
-            foreach( $images as $image ){
-               $json[] = url(Storage::url("app/public/" . $image));
+
+        if ($isApiRequest) {
+            foreach ($images as $image) {
+                $json[] = url(Storage::url("app/public/" . $image));
             }
         } else {
-            foreach( $images as $image ){
+            foreach ($images as $image) {
                 $json[] = $image;
             }
         }
-            return $json;
+
+        return $json;
     }
-    public function units(){
+    public function units()
+    {
         return $this->hasMany(Unit::class);
     }
 
-    public function compounds(){
+    public function compounds()
+    {
         return $this->hasMany(Compound::class);
     }
 
-    public function hotels(){
+    public function hotels()
+    {
         return $this->hasMany(Hotel::class);
     }
 }
