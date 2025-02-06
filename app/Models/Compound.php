ffc9e1c6bcd965c346b22c0dd63c9781e302abf2
Kareem Mohamed <kotbekareem74@gmail.com>
2025-02-06 11:47:29 +0200

    edit compound model error at 55

--- app/Models/Compound.php
+++ app/Models/Compound.php
@@ -29,33 +29,38 @@ class Compound extends Model
     public function toArray()
     {
         $attributes = parent::toArray();
-        
+
         foreach ($this->translatable as $field) {
             $attributes[$field] = $this->getTranslation($field, app()->getLocale());
         }
 
         return $attributes;
     }
-    public function getImagesAttribute($value){
-        $images = json_decode($value, true);
+    public function getImagesAttribute($value)
+    {
+        $images = json_decode($value, true) ?? []; // Ensure $images is always an array
         $json = [];
         $isApiRequest = str_contains(request()->path(), 'api');
-        if($isApiRequest){
-            foreach( $images as $image ){
-               $json[] = url(Storage::url("app/public/" . $image));
+
+        if ($isApiRequest) {
+            foreach ($images as $image) {
+                $json[] = url(Storage::url("app/public/" . $image));
             }
         } else {
-            foreach( $images as $image ){
+            foreach ($images as $image) {
                 $json[] = $image;
             }
         }
-            return $json;
+
+        return $json;
     }
-    public function units(){
+    public function units()
+    {
         return $this->hasMany(Unit::class);
     }
 
-    public function city(){
+    public function city()
+    {
         return $this->belongsTo(City::class);
     }
 }
